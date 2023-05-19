2c7f6c062066985fef057ff23b026c84b77567d9
DilyaraL <latipova8d@gmail.com>
2023-05-19 16:17:08 +0300

    Использование шаблонов страниц сайта

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)#указываем имя нашего приложения
 #__name__ указывает имя текущего файла, но можно указать и __main__
 #будет там искать шаблоны и тд
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
 
 @app.route("/")# с помощью декоратора указываем url
 @app.route("/index")#если по разным url должен выполняться один обработчик, пишем их подряд
 def index():#по которому будет отработывать обрабочик
-    return "index"
+    return render_template('index.html', title="Про Flask", menu=menu)
 
 @app.route("/about")
 def about():
-    return "<h1>О сайте</h1>"
+    return render_template('about.html', title="О сайте", menu=menu)
 
 if __name__ == '__main__':# это прописывается для запуска именно на локальном устройстве
     app.run(debug=True)# запускаем локальный веб сервер. debug=True - в браузере будем видеть все ошибки
