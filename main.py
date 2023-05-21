5ebaeec042732cc140524d8150c6d97bdf743a5c
DilyaraL <latipova8d@gmail.com>
2023-05-21 18:28:50 +0300

    Подключение внешних ресурсов и работа с формами

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
-app = Flask(__name__)#указываем имя нашего приложения
-#__name__ указывает имя текущего файла, но можно указать и __main__
-#будет там искать шаблоны и тд
+app = Flask(__name__)  # указываем имя нашего приложения
+# __name__ указывает имя текущего файла, но можно указать и __main__
+# будет там искать шаблоны и тд
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
-@app.route("/")# с помощью декоратора указываем url
-#@app.route("/index") #если по разным url должен выполняться один обработчик, пишем их подряд
-def index():#по которому будет отработывать обрабочик
-    print(url_for('index'))# получим url обработчика "/"
+
+@app.route("/")  # с помощью декоратора указываем url
+# @app.route("/index") #если по разным url должен выполняться один обработчик, пишем их подряд
+def index():  # по которому будет отработывать обрабочик
+    print(url_for('index'))  # получим url обработчика "/"
     return render_template('index.html', title="Про Flask", menu=menu)
 
 
@@ -18,11 +21,20 @@ def about():
     return render_template('about.html', title="О сайте", menu=menu)
 
 
-@app.route("/profile/<username>")#динамечкое создание url
+@app.route("/profile/<username>")  # динамечкое создание url
 def profile(username):
     return f"Пользователь: {username}"
 
 
-if __name__ == '__main__':# это прописывается для запуска именно на локальном устройстве
-    app.run(debug=True)# запускаем локальный веб сервер. debug=True - в браузере будем видеть все ошибки
-    #по оканчании разработки надо поставить False
+# надо явно указать, что можем принимать данные методом POST: methods=["POST"]
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form)
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
+if __name__ == '__main__':  # это прописывается для запуска именно на локальном устройстве
+    app.run(debug=True)  # запускаем локальный веб сервер. debug=True - в браузере будем видеть все ошибки
+    # по оканчании разработки надо поставить False
