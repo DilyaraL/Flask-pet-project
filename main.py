ecc4df4b9ca02429ed374be550b440a3db4dfb33
DilyaraL <latipova8d@gmail.com>
2023-05-21 20:55:56 +0300

    Декоратор errorhandler, функции redirect и abort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)  # указываем имя нашего приложения
 # __name__ указывает имя текущего файла, но можно указать и __main__
@@ -24,7 +24,11 @@ def about():
 
 @app.route("/profile/<username>")  # динамечкое создание url
 def profile(username):
-    return f"Пользователь: {username}"
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)#возращаем ошибку сервера, если пользователь не авторизован
+        #но хочет получить доступ к профилю
+    else:
+        return f"Пользователь: {username}"
 
 
 # надо явно указать, что можем принимать данные методом POST: methods=["POST"]
@@ -38,6 +42,21 @@ def contact():
             flash('Ошибка отправки', category='error')
     return render_template('contact.html', title="Обратная связь", menu=menu)
 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:#проверяем авторизован ли пользователь, те присутствует ли ключ 'userLogged' в сессии
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == "POST" and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404) #обрабочик ошибок
+def pageNotFount(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu), 404
 
 if __name__ == '__main__':  # это прописывается для запуска именно на локальном устройстве
     app.run(debug=True)  # запускаем локальный веб сервер. debug=True - в браузере будем видеть все ошибки
